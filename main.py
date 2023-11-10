e151aea7e54a43836ca1ca248294616417d69daa
manuredd <74529341+manuredd@users.noreply.github.com>
2023-11-09 19:55:24 -0600

    rec func made

--- main.py
+++ main.py
@@ -13,19 +13,88 @@ class SudokuBoard:
     def __init__(self, grid):
         self.grid = grid
         
+    
+        
     def rec_solve(self, row, col):
+        if row == 8 and col == 9:
+            print('Puzzle Solved!')
+            return
+        
         if self.grid[row][col] != '_':
             
             if col == 8:
-                self.rec_solve(row + 1, 0)
+                return self.rec_solve(row + 1, 0)
                 
             else:
-                self.rec_solve(row, col + 1)
-    
+                return self.rec_solve(row, col + 1)
+                
         else:
-            pass
-            #main rec ifs
-            
+            #1
+            if self.is_valid(1, row, col):
+                self.grid[row][col] = 1
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #2
+            if self.is_valid(2, row, col):
+                self.grid[row][col] = 2
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #3
+            if self.is_valid(3, row, col):
+                self.grid[row][col] = 3
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #4
+            if self.is_valid(4, row, col):
+                self.grid[row][col] = 4
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #5
+            if self.is_valid(5, row, col):
+                self.grid[row][col] = 5
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #6
+            if self.is_valid(6, row, col):
+                self.grid[row][col] = 6
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #7
+            if self.is_valid(7, row, col):
+                self.grid[row][col] = 7
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #8
+            if self.is_valid(8, row, col):
+                self.grid[row][col] = 8
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+            #9
+            if self.is_valid(9, row, col):
+                self.grid[row][col] = 9
+                if col == 8:
+                    return self.rec_solve(row + 1, 0)
+                else:
+                    return self.rec_solve(row, col + 1)
+                
+        return
+             
 
     def is_valid(self, num, row, col):
         # check box
@@ -97,6 +166,8 @@ class SudokuBoard:
         for i in self.grid:
             if i[col] == num:
                 return False
+            
+        return True
         
 if __name__ == '__main__':
     main()
